be7e715c384920197cb7020ef967e31e48e24c26
Ivan <Ivan@macbook-pro-umanets.local>
2019-02-07 15:28:36 +0200

    TASK 1

--- Assignment #3/ViewController.swift
+++ Assignment #3/ViewController.swift
@@ -13,8 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+        countOfSymbolsInName(myName: "Ivan Umanets") //task 1
+        
+    }
+    
+    func countOfSymbolsInName(myName: String) {
+        let myName = myName
+        var countOfSymbols = 0
+        for _ in myName {
+            countOfSymbols = countOfSymbols + 1
+        }
+        print ("\(countOfSymbols) symbols in your name including space between given and family names")
+    }
+    
     }
 
 
-}
+
 
