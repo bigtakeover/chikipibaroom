09433181b3a6273188e5545b57253fb8fbb8e88b
Ivan <Ivan@macbook-pro-umanets.local>
2019-02-07 15:07:45 +0200

    Initial Commit

---
+++ Assignment #3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment #3
+//
+//  Created by Ivan on 07/02/2019.
+//  Copyright © 2019 Ivan Umanets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
